59929437c0c3aa0d4e3c4a347110f49edaf6fe4c
Douwe Jongeneel <douwejongeneel@gmail.com>
2016-07-28 16:57:03 +0200

    ArtikelDAO, ArtikelDAOInterface, ArtikelDAOService + delete oudeDAOstuff

---
+++ src/main/java/database/daos/mysql/ArtikelDAO.java
@@ -0,0 +1,87 @@
+package database.daos.mysql;
+
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import database.interfaces.ArtikelDAOInterface;
+import model.Artikel;
+
+public class ArtikelDAO implements ArtikelDAOInterface<Artikel, String> {
+	
+	private EntityManagerFactory entityManagerFactory = null;
+	
+	private EntityManager createEntityManager() {
+		if (entityManagerFactory == null) {
+			entityManagerFactory = Persistence.createEntityManagerFactory("rsvierproject2PU");
+		}
+		return entityManagerFactory.createEntityManager();
+	}
+	
+	public ArtikelDAO() {
+	}
+
+	@Override
+	public void persist(Artikel entity) {
+		EntityManager entityManager = createEntityManager();
+		entityManager.getTransaction().begin();
+		entityManager.persist(entity);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		
+	}
+	
+	@Override
+	public void update(Artikel entity) {
+		EntityManager entityManager = createEntityManager();
+		entityManager.getTransaction().begin();
+		entityManager.merge(entity);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		
+	}
+	@Override
+	public Artikel findById(String id) {
+		EntityManager entityManager = createEntityManager();
+		entityManager.getTransaction().begin();
+		Artikel artikel = entityManager.find(Artikel.class, id);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		
+		return artikel;
+	}
+
+	@Override
+	public void delete(Artikel entity) {
+		EntityManager entityManager = createEntityManager();
+		entityManager.getTransaction().begin();
+		Artikel artikel = entityManager.merge(entity);
+		entityManager.remove(artikel);
+		entityManager.getTransaction().commit();
+		entityManager.close();
+	}
+
+	@Override
+	public Set<Artikel> findAll() {
+		EntityManager entityManager = createEntityManager();
+		entityManager.getTransaction().begin();
+		TypedQuery<Artikel> query = entityManager.createQuery("SELECT a FROM ARTIKEL a ", Artikel.class);
+		Set<Artikel> resultaten = (Set<Artikel>)query.getResultList();
+		entityManager.getTransaction().commit();
+		entityManager.close();
+		
+		return resultaten;
+	}
+
+	@Override
+	public void deleteALl() {
+		Set<Artikel> artikelLijst = findAll();
+		for (Artikel artikel : artikelLijst) {
+			delete(artikel);
+		}
+		
+	}
+}
---
+++ src/main/java/database/service/ArtikelService.java
@@ -0,0 +1,45 @@
+package database.service;
+
+import java.util.Set;
+
+import database.daos.mysql.ArtikelDAO;
+import model.Artikel;
+
+public class ArtikelService {
+	
+	private static ArtikelDAO artikelDAO;
+	
+	public ArtikelService() {
+		if (artikelDAO == null) {
+			artikelDAO = new ArtikelDAO();
+		}
+	}
+	
+	public void persist(Artikel entity) {
+		artikelDAO.persist(entity);
+	}
+	
+	public Artikel findById(String id){
+		return artikelDAO.findById(id);
+	}
+	
+	public void update(Artikel entity) {
+		artikelDAO.update(entity);
+	}
+	
+	public void delete(Artikel entity) {
+		artikelDAO.delete(entity);
+	}
+	
+	public Set<Artikel> findAll() {
+		return artikelDAO.findAll();
+	}
+	
+	public void deleteAll() {
+		artikelDAO.deleteALl();
+	}
+	
+	public ArtikelDAO artikelDAO() {
+		return artikelDAO;
+	}
+}
