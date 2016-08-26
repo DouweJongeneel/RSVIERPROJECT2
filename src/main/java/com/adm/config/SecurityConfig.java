0032caecebc7a4f21036cb48a83c4ed68b0ded0f
milanverheij <milan@verheij.at>
2016-08-27 00:52:47 +0200

    Security by JDBC

--- src/main/java/com/adm/config/SecurityConfig.java
+++ src/main/java/com/adm/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.adm.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.rememberme.InMemoryTokenRepositoryImpl;
+
+import javax.sql.DataSource;
 
 /**
  * Created by Milan_Verheij on 16-08-16.
@@ -19,6 +23,9 @@ import org.springframework.security.web.authentication.rememberme.InMemoryTokenR
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -45,14 +52,28 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                  .anyRequest().permitAll();
     }
 
+//    @Override
+//    @Profile("development")
+//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//                .inMemoryAuthentication()
+//                 .withUser("user").password("password").roles("USER")
+//                .and()
+//                 .withUser("Harrie").password("1234").roles("USER")
+//                .and()
+//                 .withUser("admin").password("password").roles("USER", "ADMIN");
+//    }
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
         auth
-                .inMemoryAuthentication()
-                 .withUser("user").password("password").roles("USER")
-                .and()
-                 .withUser("Harrie").password("1234").roles("USER")
-                .and()
-                 .withUser("admin").password("password").roles("USER", "ADMIN");
+                .jdbcAuthentication()
+                 .dataSource(dataSource)
+                 .usersByUsernameQuery(
+                         "select email, password, klantActief " +
+                         "from Klant where email=?")
+                 .authoritiesByUsernameQuery(
+                         "select email, 'ROLE_USER' from Klant where email=?");
     }
 }
